b2af3fa6f7af1e286c9cf4ef22ef18b8fd91ae79
Aaron Morton <aaron@thelastpickle.com>
2014-01-09 13:55:17 +1300

    remove metaclass for DB plugin management
    the metaclass required dependencies that made things difficult.
    
    Specify the DB plugin using the GRAPHITE_DATABASE_PLUGIN setting and
    "package.module.class"

--- webapp/graphite/database.py
+++ webapp/graphite/database.py
@@ -1,9 +1,6 @@
-from graphite.util import PluginRegistrar
 
 class GraphiteDatabase(object):
   "Abstract base class for Carbon database backends"
-  __metaclass__ = PluginRegistrar
-  plugins = {}
 
   def finder(self):
     raise NotImplemented()
